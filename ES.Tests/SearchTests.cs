51f92b87a4e1f9f24f6c18f394dd8adff2a205f9
Krizanac, Matt <matt.krizanac@prcm.com>
2015-04-06 16:16:56 -0500

    Added some more tests

--- ES.Tests/SearchTests.cs
+++ ES.Tests/SearchTests.cs
@@ -50,5 +50,47 @@ namespace ES.Tests
                             "Last Name: {3}" + Environment.NewLine,
                             resp.Id, resp.SeedId, resp.FirstName, resp.LastName);
         }
+
+        [Test]
+        public void SearchByFirstName()
+        {
+            /* Set Up */
+            var client = ConnectionHelper.Client;
+            client.SetUpTestIndex("test");
+
+            var hydra = new Hydrator<IndexItem>();
+            var itms = hydra.GetList(100);
+
+            client.IndexMany(itms, "test", "employees");
+
+            var rnd = new Random();
+            var itm = itms[rnd.Next(0, 99)];
+
+            Thread.Sleep(2000);
+
+            /* Execute */
+            var result = client.Search<IndexItem>(s => s.Index("test")
+                                                        .Type("employees")
+                                                        .Query(q => 
+                                                            //q.Term(p => p.FirstName, itm.FirstName) &&
+                                                            q.Term(p => p.SeedId, itm.SeedId)
+                                                         ));
+            Debug.WriteLine(itm.SeedId);
+
+
+            Assert.AreEqual((int)HttpStatusCode.OK, result.ConnectionStatus.HttpStatusCode, "Received {0} response from server", result.ConnectionStatus.HttpStatusCode);
+        }
+
+        [Test]
+        public void GetAddresses()
+        {
+            var client = ConnectionHelper.Client;
+
+            Assert.IsTrue(client.IndexExists("stateprovinces").Exists, "Test data did not exist");
+
+            var result = client.Search<Address>(x => x.Index("stateprovinces").Query(q => q.Term("stateprovinceid", 36)));
+
+            Assert.IsNotNull(result);
+        }
     }
 }
