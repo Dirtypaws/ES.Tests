a45555feed8d9fcd413d512f04a97ff4d944629f
Matthew Krizanac <matthewkrizanac@gmail.com>
2015-04-06 01:02:15 -0500

    Adding some get tests

---
+++ ES.Tests/Helpers/DataHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ES.Tests.BusinessObjects;
+using Foundation.ObjectHydrator;
+
+namespace ES.Tests.Helpers
+{
+    public static class DataHelper
+    {
+        private static IList<IndexItem> _items;
+        private static IEnumerable<IndexItem> Items
+        {
+            get
+            {
+                if (_items == null)
+                {
+                    var hydra = new Hydrator<IndexItem>();
+                    _items = hydra.GetList(100000).OrderBy(x => x.Id).ToList();
+                }
+
+                return _items;
+            }
+        }
+    }
+}
---
+++ ES.Tests/SearchTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using ES.Tests.BusinessObjects;
+using ES.Tests.Helpers;
+using Foundation.ObjectHydrator;
+using Nest;
+using NUnit.Framework;
+
+namespace ES.Tests
+{
+    public partial class DataTests
+    {
+        [Test]
+        public void GetById()
+        {
+            var client = ConnectionHelper.Client;
+            client.SetUpTestIndex("test");
+
+            var hydra = new Hydrator<IndexItem>();
+            var itms = hydra.GetList(100);
+
+            for (int i = 0; i < itms.Count -1; i++)
+                itms[i].SeedId = i + 1;
+
+            client.IndexMany(itms, "test", "employees");
+
+            Thread.Sleep(500);
+
+            var rnd = new Random();
+            var itm = itms[rnd.Next(0, 99)];
+
+            var result = client.Get<IndexItem>(x => x.Index("test").Type("employee").Id(itm.SeedId).Fields(i => i.FirstName, i=> i.LastName));
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
